d5b736f460ca4f9a57dfef14b733b5dbbf0fd03d
Avishek Goyal <avishek9197@gmail.com>
2024-08-17 17:36:50 +0530

    initial-commit

---
+++ backend.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "gcs" {
+    bucket = "dummyproject-429106-infra-backend"
+    prefix = "terraform/state"
+    credentials = ".credentials.json"
+    public_access_prevention = "enforced"
+    versioning {
+        enabled = true
+    }
+  }
+}
---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+project="dummyproject-429106"
+region="us-central1"
+zone="us-central1-c"
