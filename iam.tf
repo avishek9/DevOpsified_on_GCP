e3a869c8daf7da45b50e3737b5c41525f4013f42
Avishek Goyal <avishek9197@gmail.com>
2024-09-22 20:52:24 +0530

    added permissions for tool-machine-sa

---
+++ iam.tf
@@ -0,0 +1,5 @@
+resource "google_storage_bucket_iam_member" "member" {
+  bucket = "dummyproject-429106-infra-backend"
+  role = "roles/storage.admin"
+  member = "serviceAccount:${google_service_account.tool-machine-sa.email}"
+}
