f15de95ba46af86513fd185e7c00db7dbf271bca
Avishek Goyal <avishek9197@gmail.com>
2024-09-22 20:39:37 +0530

    fixed backend code errors

--- gke/backend.tf
+++ gke/backend.tf
@@ -2,10 +2,5 @@ terraform {
   backend "gcs" {
     bucket = "dummyproject-429106-infra-backend"
     prefix = "terraform/gke-state"
-    credentials = ".credentials.json"
-    public_access_prevention = "enforced"
-    versioning {
-        enabled = true
-    }
   }
 }
